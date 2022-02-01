b225531076206baa3ed55b034273f2905536f7e3
ervandenbosch <ezravdb@gmail.com>
2022-02-01 16:06:08 +0000

    Deploying to gh-pages from @ ervandenbosch/ervandenbosch.github.io@53c3fc0103940bcebfa742cacbe56cc299404c77 🚀

--- _next/static/4g266BcYlChWzysl9QUPL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-715078f98bf9751d.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

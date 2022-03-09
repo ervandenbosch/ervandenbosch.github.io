0e6e832cf7a458ac2baa9f3bff4e1652fa3ef79a
ervandenbosch <ezravdb@gmail.com>
2022-03-09 14:46:53 +0100

    upd

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,34 +1,43 @@
 export const projects = [
   {
     name: "NFT Marketplace",
-    description: "An NFT Marketplace for the Polygon network. Built with smart contracts and NextJS.",
+    description:
+      "An NFT Marketplace for the Polygon network. Built with smart contracts and NextJS.",
     link: "https://nft-marketplace-roan.vercel.app",
-    tags: ["nextJS", "React", "tailwindcss", "blockchain", "web3"]
+    tags: ["nextJS", "React", "tailwindcss", "blockchain", "web3"],
   },
   {
     name: "Recipe app",
     description: "A showcase of API usage to find recipes.",
     link: "https://recipe-app-two-pi.vercel.app/",
-    target:"_blank",
-    tags: ["API", "nextJS", "React", "tailwindcss"]
+    target: "_blank",
+    tags: ["API", "nextJS", "React", "tailwindcss"],
   },
   {
     name: "Drum app",
-    description: "A simple drum machine in React with claymorphism design principles.",
+    description:
+      "A simple drum machine in React with claymorphism design principles.",
     link: "https://ervandenbosch.github.io/drums/",
-    tags: ["HTML", "CSS", "Javascript", "React"]
+    tags: ["HTML", "CSS", "Javascript", "React"],
   },
   {
     name: "Calculator app",
     description: "A calculator in React with claymorphism design principles.",
     link: "https://ervandenbosch.github.io/calculator/",
-    tags: ["HTML", "CSS", "Javascript", "React"]
+    tags: ["HTML", "CSS", "Javascript", "React"],
   },
   {
     name: "Restaurant website",
     description: "Restaurant website made in CMS Wordpress.",
     link: "https://cotam.nl",
-    target:"_blank",
-    tags: ["Wordpress", "HTML", "CSS", "CMS"]
-  }
+    target: "_blank",
+    tags: ["Wordpress", "HTML", "CSS", "CMS"],
+  },
+  {
+    name: "Restaurant website",
+    description: "Restaurant website made in NextJS.",
+    link: "https://co-tam.vercel.app/",
+    target: "_blank",
+    tags: ["HTML", "CSS", "Javascript", "NextJS", "React", "Tailwind"],
+  },
 ];
